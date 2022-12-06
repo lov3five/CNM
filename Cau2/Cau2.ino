6ebc06bb296af806330e610d33678ab1fb8e6644
lov3five <luongtranstaff@gmail.com>
2022-12-06 14:44:21 +0700

    Cau 2

--- Cau2/Cau2.ino
+++ Cau2/Cau2.ino
@@ -1,6 +1,6 @@
 /*
   Author: @lov3five
-  * Device:
+    Device:
     - Arduino Uno R3
     - Gas sensor MQ135
     - 1 RED led, 1 GREEN led
@@ -16,46 +16,48 @@ int valueMQ135;
 void setup() {
   //Setup tốc độ dữ liệu theo bit trên giây (baud) để truyền dữ liệu
   Serial.begin(9600);
-
   //INPUT
   pinMode(MQ_135, INPUT);
-
   //OUTPUT
   pinMode(RED_LED, OUTPUT);
   pinMode(GREEN_LED, OUTPUT);
   pinMode(BUZZER, OUTPUT);
-
-  // Setup tắt mặc định
-  digitalWrite(LED, LOW);
-  digitalWrite(BUZZER, LOW);
 }
 
 void blinkLed3s(int pin) {
   digitalWrite(pin, HIGH);
-  delay(1000);
+  delay(500);
   digitalWrite(pin, LOW);
-  
+  delay(500);
   digitalWrite(pin, HIGH);
-  delay(1000);
+  delay(500);
   digitalWrite(pin, LOW);
-  
+  delay(500);
   digitalWrite(pin, HIGH);
-  delay(1000);
+  delay(500);
   digitalWrite(pin, LOW);
+  delay(500);
 }
 
 void loop() {
+  //Mỗi 5s nhận dữ liệu từ cảm biến 
+  delay(5000);
+  
   valueMQ135 =  analogRead(MQ_135); //Đọc giá trị nhận từ cảm biến MQ135 và gán vào valueMQ135
+  Serial.print("Gia tri MQ_135 = ");
+  Serial.println(valueMQ135);
+  
   //Nếu giá trị > 200 thì Led đỏ sáng 3s rồi tắt & còi kêu trong 5s rồi tắt
-  if (valueMQ125 > 200) {
+  if (valueMQ135 > 200) {
     digitalWrite(GREEN_LED, LOW);
+    
+    digitalWrite(BUZZER, HIGH);
     
     // Led đỏ nhấp nháy trong 3s
     blinkLed3s(RED_LED);
 
-    //Còi kêu trong 5s
-    digitalWrite(BUZZER, HIGH);
-    delay(5000);
+    //Còi kêu trong 5s (trừ 3s của LED thì còn 2s)
+    delay(2000);
     digitalWrite(BUZZER, LOW);
   } else {
     digitalWrite(RED_LED, LOW);
