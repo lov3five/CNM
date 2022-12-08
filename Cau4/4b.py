a1ce07c405fd6db72aa70c40fe960f5ed48cede5
lov3five <luongtranstaff@gmail.com>
2022-12-08 12:35:13 +0700

    Cau

--- Cau4/4b.py
+++ Cau4/4b.py
@@ -5,11 +5,21 @@ import time
 # Setmode BCM
 GPIO.setmode(GPIO.BCM)
 # Chân Pin cảm biến MQ135
-MQ135_PIN = 4
+MQ135_PIN = 4 # = 7 BOARD
 RED_PIN = 17 # = 11 BOARD
 # INPUT
-GPIO.setup(MQ135_PIN, GPIO.IN) # = 7 BOARD
+GPIO.setup(MQ135_PIN, GPIO.IN) 
 GPIO.setup(RED_PIN, GPIO.OUT)
+
+while True:
+    valueMQ135 = GPIO.input(MQ135_PIN)
+    print('Value of MQ135: {valueMQ135}')
+    if(valueMQ135 > 60):
+        GPIO.output(RED_PIN, GPIO.HIGH)
+        print("RED_LED turn ON")
+    time.sleep(5)
+
+
 
 
 
--- Cau4/4c.py
+++ Cau4/4c.py
@@ -1,1 +1,31 @@
-""" Thu thập dữ liệu ở khu vực B """
+""" 
+Khu vực B 
+Thu thập dữ liệu theo dõi chuyển động và có cảnh báo led đỏ nhấp nháy
+"""
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+PIR_PIN = 4 # = 7 BOARD
+RED_PIN = 17 # = 11 BOARD
+#Left : 5V
+#Middle: OUT
+#Right: GND
+GPIO.setup(PIR_PIN, GPIO.IN)
+GPIO.setup(RED_PIN, GPIO.OUT)
+
+def blinkLed(pin, duration):
+     GPIO.output(pin, GPIO.HIGH)
+     time.sleep(duration/2)
+     GPIO.output(pin, GPIO.LOW)
+     time.sleep(duration/2)
+
+while True:
+        time.sleep(0.1) 
+        if GPIO.input(PIR_PIN)==1 :
+            print (GPIO.input(PIR_PIN))
+            print ("Motion detected!!!")
+            blinkLed(RED_PIN, 1)
+        elif GPIO.input(PIR_PIN)==0:
+            print ("No motion.")
+            GPIO.output(RED_PIN,GPIO.LOW)
+            
